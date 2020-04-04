ae604e4eec13fd3d11f73f322e0a2bb8c27546ce
LUOYUC98 <48544752+LUOYUC98@users.noreply.github.com>
2020-04-04 18:16:28 +1100

    Create debug.c

---
+++ structure/debug.c
@@ -0,0 +1,26 @@
+#include "common.h"
+
+
+void print_cached_web_addr(){
+  printf("---------web_addr-------\n");
+  for(int i = 0; i < addr_index; i++){
+    printf("%dth addr: %s\n", i, web_addr[i]);
+  }
+    printf("----------------\n");
+}
+
+
+void print_parent_addr(){
+    printf("---------parent_addr-------\n");
+    printf("%s\n", parent_addr_buffer);
+    printf("----------------\n");
+}
+
+void print_html_buffer(){
+  printf("---------parent_addr-------\n");
+  if(strlen(parent_addr_buffer) == 0){
+    printf("html buffer cleaned\n");
+  }else{    printf("html buffer not cleaned\n");}
+  printf("----------------\n");
+}
+
