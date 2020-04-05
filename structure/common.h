2a1226adcbc7ee209fa7e6700739b459176694c8
LUOYUC98 <48544752+LUOYUC98@users.noreply.github.com>
2020-04-05 22:42:28 +1000

    Update common.h

--- structure/common.h
+++ structure/common.h
@@ -10,7 +10,6 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <ctype.h>
-#include<pcre.h>
 #include <regex.h>
 #define MAX_URL_NUM 100
 #define MAX_RESPONSE 10000
@@ -26,7 +25,7 @@ extern char* p_html_buffer;
 extern char*web_addr[MAX_URL_NUM+1];
 extern int addr_index;
 extern int addr_fetching_index;
-extern int html_cursor;
+
 extern char parent_addr_buffer[MAX_URL_LEN+1];
 extern char input[MAX_DOMAIN_LEN+1];
 
@@ -36,7 +35,7 @@ int socket_connect(char *host, in_port_t port);
 
 //fetch.c
 char* generateGET(char* path, char* addr, char*user_agent);
-int get_response(int fd, char* path, char* domain, char* port_number, char* user_agent);
+void get_response(char* path, char* domain, char* port_number, char* user_agent);
 char* get_html_start();
 int get_status_code();
 int contain_punc(char* path);
@@ -47,6 +46,7 @@ int check_duplicate(char* url);
 int need_crawl(char* url);
 int not_truncated();
 int content_type_is_text();
+char* get_parent_url(char* url);
 //html_phase.c
 void extract_matches();
 
