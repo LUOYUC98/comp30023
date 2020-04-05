b4021eed867471eff332cffad8588e250b1c6257
LUOYUC98 <48544752+LUOYUC98@users.noreply.github.com>
2020-04-05 22:42:50 +1000

    Update crawler.c

--- structure/crawler.c
+++ structure/crawler.c
@@ -13,21 +13,6 @@ char input[MAX_DOMAIN_LEN+1] = "web1.comp30023";
 
 
 int main(int argc, char *argv[]){
-	/*int fd;
-	char buffer[BUFFER_SIZE];
-
-	if(argc < 3){
-		fprintf(stderr, "Usage: %s <hostname> <port>\n", argv[0]);
-		exit(1); 
-	}
-  */
- // char* request = generateGET("", argv[1], "luoyuc");
-     
-     
- 
-  //get_response(fd, "", argv[1], argv[2], "luoyuc");
-  //get_html_start();
- // get_status_code();
 
  web_addr[0] = "web1.comp30023";
  
@@ -43,9 +28,8 @@ if(fp != NULL)
     }
     fclose(fp);
 }
-printf("len = %lu\n", strlen(get_html_start()));
- extract_matches();
- print_cached_web_addr();
+printf("len = %lu\n", strlen(html_buffer));
+ 
 
 	return 0;
 }
