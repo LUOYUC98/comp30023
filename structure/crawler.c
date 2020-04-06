ab8c886f03b4f314f685ef476feef326b910e70e
LUOYUC98 <48544752+LUOYUC98@users.noreply.github.com>
2020-04-06 17:50:13 +1000

    Update crawler.c

--- structure/crawler.c
+++ structure/crawler.c
@@ -2,34 +2,28 @@
 
 
  //extern initilization
-char html_buffer[MAX_RESPONSE+1];
+char html_buffer[5*MAX_RESPONSE+1];
 char *p_html_buffer = html_buffer;
 char *web_addr[MAX_URL_NUM+1];
 int addr_index = 0;
 int addr_fetching_index = 0;
-int html_cursor = 0;
+char url_buffer[MAX_URL_LEN+1];
 char parent_addr_buffer[MAX_URL_LEN+1];
-char input[MAX_DOMAIN_LEN+1] = "web1.comp30023";
-
+char input[MAX_DOMAIN_LEN+1];
+char request[200];
 
 int main(int argc, char *argv[]){
+	
+  
+	if(argc < 2){
+		fprintf(stderr, "Usage: %s <hostname> \n", argv[0]);
+		exit(1); 
+	}
+  
+  strcpy(input, url_ok(argv[1]));
+  web_addr[addr_index] = url_ok(argv[1]);
+  sequential_visit();
 
- web_addr[0] = "web1.comp30023";
- 
- FILE* fp = fopen("html.txt", "r");
- 
- char symbol;
- int count = 0;
-if(fp != NULL)
-{
-    while((symbol = getc(fp)) != EOF)
-    {
-        html_buffer[count++] = symbol;
-    }
-    fclose(fp);
-}
-printf("len = %lu\n", strlen(html_buffer));
- 
 
 	return 0;
 }
