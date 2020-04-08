21697fe65991e93c3f9a8bd37e6c3bf4b4f09a22
LUOYUC98 <48544752+LUOYUC98@users.noreply.github.com>
2020-04-08 15:07:17 +1000

    Update fetch.c

--- structure/fetch.c
+++ structure/fetch.c
@@ -3,41 +3,57 @@
 
 
 
-void generateGET(char* path, char* addr, char*user_agent){
+void generateGET(char* path, char* addr, char*user_agent, int auth){
+  //printf("auth  = %d\n", auth);
+  if(auth == 0){
     sprintf(request, "GET /%s HTTP/1.0\r\nHost: %s\r\nUser-Agent: %s\r\nContent-Type: text/plain\r\n\r\n", path, addr, user_agent);
+    return;
+  }
+  if(auth == 1){
+  sprintf(request, "GET /%s HTTP/1.1\r\nHost: %s\r\nUser-Agent: %s\r\nContent-Type: text/plain\r\nAuthorization: Basic Z3Vlc3Q6Z3Vlc3Q=\r\n\r\n", path, addr, user_agent);
+  return;
+  }
 }
 
   
-  int get_response( char* path, char* domain, char* port_number, char* user_agent){
-  
+  int get_response( char* domain){
+ 
   char buffer[BUFF_SIZE+1];
-  generateGET(path, domain, user_agent);
-	int fd = socket_connect(domain, atoi(port_number)); 
+  //printf("\n\n%s\n\n", request);
+	int fd = socket_connect(domain, 80); 
 	write(fd, request, strlen(request)); 
 	bzero(buffer, BUFF_SIZE);
-  bzero(request, 200);
-	//printf("\n\n%s\n\n", request);
+  bzero(request, strlen(request));
+	int status_Code;
   
   
 	while(read(fd, buffer, BUFF_SIZE) != 0){
     strcpy(p_html_buffer, buffer);
     p_html_buffer += strlen(buffer);
-    fprintf(stderr, "%s", buffer);
+   // fprintf(stderr, "%s", buffer);
 		bzero(buffer, BUFF_SIZE);
-    if(p_html_buffer - html_buffer >49000){break;}
-	}
+    
+    if(strlen(html_buffer) >49000){
+      //printf("overload!!!!\n");
+      break;
+      status_Code = 0;
+    }
+  // read(fd, html_buffer, strlen(html_buffer)) 
+
+//printf("length:%zu\n",strlen(html_buffer));
+	
   //char headerResponse[300];
   //int len = get_html_start(html_buffer) - html_buffer;
- // strncpy(headerResponse, html_buffer, len);
- // printf("\n\n%s\n", get_html_start(html_buffer));
-  
-  
+  //strncpy(headerResponse, html_buffer, len);
+  //printf("\n\n%s\n", get_html_start(html_buffer));
+  }
+//  printf("length:%zu\n",strlen(html_buffer));
+
 	shutdown(fd, SHUT_RDWR); 
 	close(fd); 
-  int status_Code = get_status_code();
-  if(p_html_buffer - html_buffer >49000){
-    status_Code = 404;
-  }
+  status_Code = get_status_code();
+ 
+  
   p_html_buffer = html_buffer;
   return status_Code;
   }
